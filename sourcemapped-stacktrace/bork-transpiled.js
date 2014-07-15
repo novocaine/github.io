c12cdd42b8f3b84907a97878bbe509087d6ebaaa
James Salter <iteration@gmail.com>
2014-07-15 22:00:34 +1000

    add smst demo

---
+++ sourcemapped-stacktrace/bork-transpiled.js
@@ -0,0 +1,6 @@
+function runBork() {
+  var msg = arguments[0] !== (void 0) ? arguments[0] : "default msg";
+  throw new Error("I have borked from transpiled code.");
+}
+
+//# sourceMappingURL=bork-transpiled.map
---
+++ sourcemapped-stacktrace/bork.js
@@ -0,0 +1,3 @@
+function runBork(msg="default msg") { 
+  throw new Error("I have borked from transpiled code.");
+}
