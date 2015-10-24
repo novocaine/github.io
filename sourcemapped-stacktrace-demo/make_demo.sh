8da1c1ba267dc5d9827c38cc9a2fce505386460f
James Salter <iteration@gmail.com>
2015-10-24 17:58:33 +0100

    add .map

--- sourcemapped-stacktrace-demo/make_demo.sh
+++ sourcemapped-stacktrace-demo/make_demo.sh
@@ -1,4 +1,4 @@
 pushd ../sourcemapped-stacktrace/demo
 sh make.sh
-cp *.coffee *.js *.html ../../sourcemapped-stacktrace-demo/public_html
+cp *.coffee *.js *.html *.map ../../sourcemapped-stacktrace-demo/public_html
 popd
