ff4edc3a3b24490002231da3d32da1e12c088b9a
James Salter <iteration@gmail.com>
2015-10-24 17:57:49 +0100

    include .coffee

--- sourcemapped-stacktrace-demo/make_demo.sh
+++ sourcemapped-stacktrace-demo/make_demo.sh
@@ -1,4 +1,4 @@
 pushd ../sourcemapped-stacktrace/demo
 sh make.sh
-cp *.js *.html ../../sourcemapped-stacktrace-demo/public_html
+cp *.coffee *.js *.html ../../sourcemapped-stacktrace-demo/public_html
 popd
