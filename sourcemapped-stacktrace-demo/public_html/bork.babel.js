5325c853e5171c9fddabe7c37440306275c2119c
James Salter <iteration@gmail.com>
2016-07-06 12:57:59 +0100

    update smst; update babel output

--- sourcemapped-stacktrace-demo/public_html/bork.babel.js
+++ sourcemapped-stacktrace-demo/public_html/bork.babel.js
@@ -1,26 +1,9 @@
-"use strict";
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var BabelBorker = (function () {
-    function BabelBorker() {
-        _classCallCheck(this, BabelBorker);
+class BabelBorker {
+    bork() {
+        throw new Error("bork from es6");
     }
+}
 
-    _createClass(BabelBorker, [{
-        key: "bork",
-        value: function bork() {
-            throw new Error("bork from es6");
-        }
-    }]);
+window.babel_bork = () => new BabelBorker().bork();
 
-    return BabelBorker;
-})();
-
-window.babel_bork = function () {
-    return new BabelBorker().bork();
-};
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvcmsuZXM2Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztJQUFNLFdBQVc7YUFBWCxXQUFXOzhCQUFYLFdBQVc7OztpQkFBWCxXQUFXOztlQUNULGdCQUFHO0FBQ0gsa0JBQU0sSUFBSSxLQUFLLENBQUMsZUFBZSxDQUFDLENBQUM7U0FDcEM7OztXQUhDLFdBQVc7OztBQU1qQixNQUFNLENBQUMsVUFBVSxHQUFHO1dBQU0sSUFBSSxXQUFXLEVBQUUsQ0FBQyxJQUFJLEVBQUU7Q0FBQSxDQUFBIiwiZmlsZSI6InVuZGVmaW5lZCIsInNvdXJjZXNDb250ZW50IjpbImNsYXNzIEJhYmVsQm9ya2VyIHtcbiAgICBib3JrKCkge1xuICAgICAgICB0aHJvdyBuZXcgRXJyb3IoXCJib3JrIGZyb20gZXM2XCIpO1xuICAgIH1cbn1cblxud2luZG93LmJhYmVsX2JvcmsgPSAoKSA9PiBuZXcgQmFiZWxCb3JrZXIoKS5ib3JrKClcbiJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvcmsuZXM2Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLE1BQU0sV0FBTixDQUFrQjtBQUNkLFdBQU87QUFDSCxjQUFNLElBQUksS0FBSixDQUFVLGVBQVYsQ0FBTjtBQUNIO0FBSGE7O0FBTWxCLE9BQU8sVUFBUCxHQUFvQixNQUFNLElBQUksV0FBSixHQUFrQixJQUFsQixFQUExQiIsImZpbGUiOiJzdGRvdXQiLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBCYWJlbEJvcmtlciB7XG4gICAgYm9yaygpIHtcbiAgICAgICAgdGhyb3cgbmV3IEVycm9yKFwiYm9yayBmcm9tIGVzNlwiKTtcbiAgICB9XG59XG5cbndpbmRvdy5iYWJlbF9ib3JrID0gKCkgPT4gbmV3IEJhYmVsQm9ya2VyKCkuYm9yaygpXG4iXX0=
